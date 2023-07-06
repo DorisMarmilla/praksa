d11010e1439f8696e6aaf8d3894e0b29a9fcce07
˝Doris <marmilladdd@gmail.com>
2023-07-06 14:44:55 +0200

    Cetvrti dan prakse

--- script.js
+++ script.js
@@ -1,29 +1,52 @@
 
 const carousel = document.querySelector(".carousel");
 
-let isDragStart = false, prevPageX , prevScrollLeft;
+let isDragStart = false, prevPageX, prevScrollLeft;
 
 const dragStart = (e) => {
     isDragStart = true;
     prevPageX = e.pageX;
-    prevScrollLeft=carousel.scrollLeft;
+    prevScrollLeft = carousel.scrollLeft;
 }
 const dragging = (e) => {
     if (!isDragStart) return;
     e.preventDefault();
-    let positionDiff= e.pageX - prevPageX;
-    carousel.scrollLeft = prevScrollLeft-positionDiff;
-    
+    let positionDiff = e.pageX - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+
 }
 
 const dragStop = () => {
     isDragStart = false;
-   
+
 }
 
 carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
 carousel.addEventListener("mouseup", dragStop);
 
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
 
-  
+myButton.addEventListener("click", function () {
+    myPopup.classList.add("show");
+});
+closePopup.addEventListener("click", function () {
+    myPopup.classList.remove("show");
+});
+window.addEventListener("click", function (event) {
+    if (event.target == myPopup) {
+        myPopup.classList.remove("show");
+    }
+});
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+
